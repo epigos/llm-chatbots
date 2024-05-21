ddb9d7b04ce234893ea17aa51eb90dbcdd4054d5
Philip Adzanoukpe <epigos@gmail.com>
2024-05-21 16:17:38 +0000

    chore: fix unique bot names fixtures

--- tests/factories.py
+++ tests/factories.py
@@ -1,13 +1,16 @@
 import uuid
 
 import factory
+from faker import Faker
 
 from app import models
+
+fake = Faker()
 
 
 class BotFactory(factory.Factory):
     id = factory.LazyFunction(uuid.uuid4)
-    name = factory.Faker("word")
+    name = factory.Sequence(lambda n: f"{fake.word()}-{n}")
     welcome_message = factory.Faker("sentence")
 
     class Meta:
